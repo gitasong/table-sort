859219a7b05281ceb23b685cc50d25e131f36074
Nicole Freed <nicole.freed@gmail.com>
2018-12-27 18:00:35 -0800

    stub out app logic

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header"></header>
+        <header as="h1">Title Sort</header>
+        {/* filter/input component - fetch and filter data based on input string */}
+        {/* table component - conditionally render based on results of input filtering */}
+          {/* table row component - render for each returned data object */}
       </div>
     );
   }
