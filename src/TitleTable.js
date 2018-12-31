2eaadc0e08ac6d3c3d064a112f4d60fed5861886
Nicole Freed <nicole.freed@gmail.com>
2018-12-31 00:28:29 -0800

    cleanup & delete unnecessary code

--- src/TitleTable.js
+++ src/TitleTable.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import TitleRow from './TitleRow';
 import { Table } from 'semantic-ui-react';
 
 class TitleTable extends Component {
@@ -10,12 +9,11 @@ class TitleTable extends Component {
     // console.log(`data.length = ${this.props.data.length}`);
 
     const titleList = data.map((item) =>
-      // <TitleRow item={item} key={item.id} />
       <Table.Row key={item.id}>
         <Table.Cell><p>{item.userId}</p></Table.Cell>
         <Table.Cell><p>{item.id}</p></Table.Cell>
         <Table.Cell><p>{item.title}</p></Table.Cell>
-      <Table.Cell><p>{`${item.completed}`}</p></Table.Cell>
+        <Table.Cell><p>{`${item.completed}`}</p></Table.Cell>
       </Table.Row>
     );
     // console.log(`titleList: ${titleList}`);
@@ -34,7 +32,6 @@ class TitleTable extends Component {
         </Table.Header>
 
         <Table.Body>
-          {/* {this.titleList.length > 0 ? {titleList} : null} */}
           {titleList}
         </Table.Body>
 
