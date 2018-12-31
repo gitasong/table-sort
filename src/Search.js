a96deb576edfc9f439c37da2f07d4ca029be8765
Nicole Freed <nicole.freed@gmail.com>
2018-12-31 00:07:34 -0800

    delete and rename old props

--- src/Search.js
+++ src/Search.js
@@ -4,14 +4,14 @@ import { Form } from 'semantic-ui-react';
 class Search extends Component {
   render() {
     return(
-      <Form onSubmit={this.props.filterSearchTerm}>
+      <Form>
         <Form.Input
           fluid
           size="large"
           icon="search"
           label="Search for a Title"
-          placeholder="Enter search text (Press Return to display results)"
-          onChange={this.props.updateSearchTerm}
+          placeholder="Enter search text (Press Return to search)"
+          onChange={this.props.filterSearch}
         />
       </Form>
     )
